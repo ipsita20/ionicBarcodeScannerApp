047c6b7746a882d5b4c849ecdc17e593cb58bdaa
Ipsita Dutta <ipsita@optimizeitsystems.com>
2019-05-29 13:00:50 +0530

    changes in home.page.ts

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 
 import { HomePage } from './home.page';
 
@@ -24,27 +23,6 @@ import { HomePage } from './home.page';
 })
 export class HomePageModule {
 
-  constructor(private barcodeScanner: BarcodeScanner, private camera: Camera) {}
-
-  image: any = '';
-  openCam() {
-
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-    this.camera.getPicture(options).then((imageData) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64 (DATA_URL):
-     alert(imageData);
-     this.image = ( window as any ).Ionic.WebView.convertFileSrc(imageData);
-    }, (err) => {
-     // Handle error
-     alert('error ' + JSON.stringify(err));
-    });
-
-  }
+  constructor() {}
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private barcodeScanner: BarcodeScanner, private camera: Camera) {
+  }
+  image: any = '';
+  openCam(): void {
+    console.log('camera');
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     alert(imageData);
+     this.image = ( window as any ).Ionic.WebView.convertFileSrc(imageData);
+    }, (err) => {
+     // Handle error
+     alert('error ' + JSON.stringify(err));
+    });
 
+  }
 }
